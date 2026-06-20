888ca3dd96d7b43ea8cf3f41a285f470cc4beb2c
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-06-20 00:14:10 -0400

    Refactor tests to individually create stacks
    It's easier to reason about the behavior of each of the tests with the resources they need collocated with them, instead of having them shared across all the tests.

--- lab1/lab1/tests/test_stack.py
+++ lab1/lab1/tests/test_stack.py
@@ -2,36 +2,38 @@ import unittest
 import lab1.stack as s
 
 class TestStack(unittest.TestCase):
-    def setUp(self):
-        print("setting up stack for tests")
-        self.stack = s.Stack() # unbounded
-        self.bounded_stack = s.Stack(10) # bounded
-
     def test_new_stack_is_empty(self):
-        self.assertTrue(self.stack.is_empty())
-        self.assertTrue(self.bounded_stack.is_empty())
+        stack = s.Stack() # unbounded
+        bounded_stack = s.Stack(10) # bounded
+        self.assertTrue(stack.is_empty())
+        self.assertTrue(bounded_stack.is_empty())
     
     def test_new_stack_is_not_full(self):
-        self.assertFalse(self.stack.is_full())
-        self.assertFalse(self.bounded_stack.is_full())
+        stack = s.Stack() # unbounded
+        bounded_stack = s.Stack(10) # bounded
+        self.assertFalse(stack.is_full())
+        self.assertFalse(bounded_stack.is_full())
     
     def test_empty_stack_no_longer_empty_after_push(self):
-        self.stack.push("a")
-        self.assertFalse(self.stack.is_empty())
+        stack = s.Stack() # unbounded
+        stack.push("a")
+        self.assertFalse(stack.is_empty())
 
     def test_push_returns_true_if_successful(self):
-        self.assertTrue(self.stack.push("a"))
+        stack = s.Stack() # unbounded
+        self.assertTrue(stack.push("a"))
     
     def test_push_returns_false_if_stack_has_0_capacity(self):
         zero_stack = s.Stack(0)
         self.assertFalse(zero_stack.push("a"))
     
     def test_push_returns_false_if_stack_is_full(self):
+        bounded_stack = s.Stack(10) # bounded
         for i in range(0,10):
-            self.bounded_stack.push("a")
-        self.assertEqual(self.bounded_stack.height, 10)
-        self.assertEqual(self.bounded_stack.max_height, 10)
-        self.assertFalse(self.bounded_stack.push("a"))
+            bounded_stack.push("a")
+        self.assertEqual(bounded_stack.height, 10)
+        self.assertEqual(bounded_stack.max_height, 10)
+        self.assertFalse(bounded_stack.push("a"))
 
 
 
