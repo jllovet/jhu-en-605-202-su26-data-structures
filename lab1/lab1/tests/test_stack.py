9a42cf295cf62fc467f0b973fc9f67b35b771ad8
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-06-20 00:58:00 -0400

    Apply linter

--- lab1/lab1/tests/test_stack.py
+++ lab1/lab1/tests/test_stack.py
@@ -1,26 +1,27 @@
 import unittest
 import lab1.stack as s
 
+
 class TestStack(unittest.TestCase):
     def test_new_stack_is_empty(self):
-        stack = s.Stack() # unbounded
-        bounded_stack = s.Stack(10) # bounded
+        stack = s.Stack()  # unbounded
+        bounded_stack = s.Stack(10)  # bounded
         self.assertTrue(stack.is_empty())
         self.assertTrue(bounded_stack.is_empty())
-    
+
     def test_new_stack_is_not_full(self):
-        stack = s.Stack() # unbounded
-        bounded_stack = s.Stack(10) # bounded
+        stack = s.Stack()  # unbounded
+        bounded_stack = s.Stack(10)  # bounded
         self.assertFalse(stack.is_full())
         self.assertFalse(bounded_stack.is_full())
-    
+
     def test_empty_stack_no_longer_empty_after_push(self):
-        stack = s.Stack() # unbounded
+        stack = s.Stack()  # unbounded
         stack.push("a")
         self.assertFalse(stack.is_empty())
-    
+
     def test_stack_becomes_full_when_max_height_is_reached(self):
-        stack = s.Stack(1) # bounded
+        stack = s.Stack(1)  # bounded
         self.assertFalse(stack.is_full())
         self.assertTrue(stack.is_empty())
         stack.push("a")
@@ -28,39 +29,38 @@ class TestStack(unittest.TestCase):
         self.assertFalse(stack.is_empty())
 
     def test_push_returns_None_if_successful(self):
-        stack = s.Stack() # unbounded
+        stack = s.Stack()  # unbounded
         self.assertIsNone(stack.push("a"))
-    
+
     def test_push_increments_stack_height(self):
-        stack = s.Stack() # unbounded
-        self.assertEqual(stack.height,0)
+        stack = s.Stack()  # unbounded
+        self.assertEqual(stack.height, 0)
         stack.push("a")
-        self.assertEqual(stack.height,1)
-    
+        self.assertEqual(stack.height, 1)
+
     def test_push_returns_false_if_stack_has_0_capacity(self):
         zero_stack = s.Stack(0)
         with self.assertRaises(OverflowError):
             zero_stack.push("a")
-    
+
     def test_push_returns_false_if_stack_is_full(self):
-        bounded_stack = s.Stack(10) # bounded
-        for i in range(0,10):
+        bounded_stack = s.Stack(10)  # bounded
+        for i in range(0, 10):
             bounded_stack.push("a")
         self.assertEqual(bounded_stack.height, 10)
         self.assertEqual(bounded_stack.max_height, 10)
         with self.assertRaises(OverflowError):
             bounded_stack.push("a")
-    
+
     def test_push_on_full_stack_raises_overflow_error(self):
-        bounded_stack = s.Stack(2) # bounded
+        bounded_stack = s.Stack(2)  # bounded
         bounded_stack.push("a")
         bounded_stack.push("b")
         with self.assertRaises(OverflowError):
             bounded_stack.push("c")
 
-
     def test_push_on_full_stack_does_not_change_height(self):
-        bounded_stack = s.Stack(2) # bounded
+        bounded_stack = s.Stack(2)  # bounded
         bounded_stack.push("a")
         bounded_stack.push("b")
         self.assertEqual(bounded_stack.max_height, 2)
@@ -69,11 +69,10 @@ class TestStack(unittest.TestCase):
         try:
             bounded_stack.push("c")
         except OverflowError:
-            pass    
+            pass
         self.assertEqual(bounded_stack.max_height, 2)
         self.assertEqual(bounded_stack.height, 2)
         self.assertEqual(bounded_stack.max_height, bounded_stack.height)
-
 
     def test_pop_returns_element_from_top_of_non_empty_stack(self):
         stack = s.Stack()
@@ -83,19 +82,20 @@ class TestStack(unittest.TestCase):
     def test_pop_returns_None_from_top_of_empty_stack(self):
         stack = s.Stack()
         self.assertIsNone(stack.pop())
-    
+
     def test_pop_decrements_stack_height(self):
         stack = s.Stack()
         stack.push("a")
         self.assertEqual(stack.height, 1)
         stack.pop()
         self.assertEqual(stack.height, 0)
-    
+
     def test_pop_on_empty_does_not_affect_height(self):
         stack = s.Stack()
         self.assertEqual(stack.height, 0)
         stack.pop()
         self.assertEqual(stack.height, 0)
+
 
 if __name__ == "__main__":
     unittest.main()
