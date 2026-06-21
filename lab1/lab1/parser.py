6a191626fa3c75711a5a472582a7e719096cafd5
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-06-21 00:06:49 -0400

    Add validation functions and tests

--- lab1/lab1/parser.py
+++ lab1/lab1/parser.py
@@ -1,3 +1,6 @@
+from lab1.stack import Stack
+
+
 def lookup_symbol(symbol: str) -> int | str:
     if not symbol:
         return ""
@@ -14,16 +17,112 @@ def parse(expression) -> list:
     return parsed_symbols
 
 
+def execute_operation(s: str, a: int, b: int) -> int:
+    match s:
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case "*":
+            return a * b
+        case "/":
+            return a // b
+        case "$":
+            return a ** b
+    raise ValueError(
+        f"Unable to execute operation {a}{s}{b}, because {s} was not recognized")
+
+
 def is_valid_prefix_expression(parsed_expression: list) -> bool:
-    if len(parsed_expression) == 1 and isinstance(parsed_expression[0], int):
+    # while input, read expression from right
+    # if operand push onto stack
+    # if operation
+    #    pop A
+    #    pop B
+    #    perform operation // A op B
+    #    push result
+    if len(parsed_expression) == 1:
+        return isinstance(parsed_expression[0], int)
+    elif len(parsed_expression) > 1:
+        if isinstance(parsed_expression[0], int):
+            return False
+    num_operations = 0
+    num_operands = 0
+    stack = Stack()
+    for i in range(0, len(parsed_expression)):  # iterate from right
+        index = len(parsed_expression) - (i + 1)
+        s = parsed_expression[index]
+        if s == ")":
+            stack.push(s)
+        if s == "(":
+            if stack.peek() == ")":
+                _ = stack.pop()
+            else:
+                return False  # unbalanced
+        if isinstance(s, int):
+            num_operands += 1
+            stack.push(s)
+        if s == "+" or s == "-" or s == "/" or s == "*" or s == "$":
+            a = stack.pop()
+            b = stack.pop()
+            if not isinstance(a, int) or not isinstance(b, int):
+                return False
+            res = execute_operation(s, a, b)  # type: ignore
+            stack.push(res)
+            num_operations += 1
+    if stack.height == 1 and isinstance(stack.peek(), int):
         return True
-    return False
+    if not stack.is_empty():
+        return False
+    if stack.is_empty() and num_operations == 0 and num_operands <= 1:
+        return True
+    if num_operations != (num_operands - 1):
+        return False
+    return True
 
 
 def is_valid_postfix_expression(parsed_expression: list) -> bool:
-    if len(parsed_expression) == 1 and isinstance(parsed_expression[0], int):
+    # while input, read expression from left
+    # if operand push onto stack
+    # if operation
+    #    pop A
+    #    pop B
+    #    perform operation // B op A
+    #    push result
+    if len(parsed_expression) == 1:
+        return isinstance(parsed_expression[0], int)
+    num_operations = 0
+    num_operands = 0
+    stack = Stack()
+    for index in range(0, len(parsed_expression)):  # iterate from left
+        s = parsed_expression[index]
+        if s == "(":
+            stack.push(s)
+        if s == ")":
+            if stack.peek() == "(":
+                _ = stack.pop()
+            else:
+                return False  # unbalanced
+        if isinstance(s, int):
+            num_operands += 1
+            stack.push(s)
+        if s == "+" or s == "-" or s == "/" or s == "*" or s == "$":
+            a = stack.pop()
+            b = stack.pop()
+            if not isinstance(a, int) or not isinstance(b, int):
+                return False
+            res = execute_operation(s, a, b)  # type: ignore
+            stack.push(res)
+            num_operations += 1
+    if stack.height == 1 and isinstance(stack.peek(), int):
         return True
-    return False
+    if not stack.is_empty():
+        return False
+    if stack.is_empty() and num_operations == 0 and num_operands <= 1:
+        return True
+    if num_operations != (num_operands - 1):
+        return False
+    return True
 
 
 def evaluate_postfix(expression: str) -> int | None:
--- lab1/lab1/tests/test_parser.py
+++ lab1/lab1/tests/test_parser.py
@@ -46,6 +46,104 @@ class TestParser(unittest.TestCase):
         self.assertListEqual(["(", "+"], parser.parse("(+"))
 
 
+class TestIsValidPrefixExpression(unittest.TestCase):
+    def test_is_valid_prefix_expression_returns_false_if_operand_to_operation_ratio_is_invalid(self):
+        self.assertFalse(
+            parser.is_valid_prefix_expression(parser.parse("++AA")))
+        self.assertFalse(
+            parser.is_valid_prefix_expression(parser.parse("++A")))
+        self.assertFalse(
+            parser.is_valid_prefix_expression(parser.parse("+")))
+
+    def test_is_valid_prefix_expression_returns_true_if_operand_to_operation_ratio_is_valid(self):
+        self.assertTrue(
+            parser.is_valid_prefix_expression(parser.parse("+AA")))
+
+    def test_is_valid_prefix_expression_returns_false_if_first_character_is_operand(self):
+        self.assertFalse(
+            parser.is_valid_prefix_expression(parser.parse("AA+")))
+        self.assertFalse(
+            parser.is_valid_prefix_expression(parser.parse("A+A")))
+
+    def test_is_valid_prefix_expression_returns_true_for_empty_parentheses(self):
+        self.assertTrue(
+            parser.is_valid_prefix_expression(parser.parse("()")))
+
+    def test_is_valid_prefix_expression_returns_false_for_unbalanced_parentheses(self):
+        self.assertFalse(
+            parser.is_valid_prefix_expression(parser.parse("(()")))
+        self.assertFalse(
+            parser.is_valid_prefix_expression(parser.parse("()(")))
+        self.assertFalse(parser.is_valid_prefix_expression(
+            parser.parse("()(()")))
+
+
+class TestIsValidPostfixExpression(unittest.TestCase):
+    def test_is_valid_postfix_expression_returns_false_if_operand_to_operation_ratio_is_invalid(self):
+        self.assertFalse(
+            parser.is_valid_postfix_expression(parser.parse("AA++")))
+        self.assertFalse(
+            parser.is_valid_postfix_expression(parser.parse("A++")))
+        self.assertFalse(
+            parser.is_valid_postfix_expression(parser.parse("+")))
+
+    def test_is_valid_postfix_expression_returns_true_if_operand_to_operation_ratio_is_valid(self):
+        self.assertTrue(
+            parser.is_valid_postfix_expression(parser.parse("AA+")))
+
+    def test_is_valid_postfix_expression_returns_false_if_first_character_is_operation(self):
+        self.assertFalse(
+            parser.is_valid_postfix_expression(parser.parse("+AA")))
+
+    def test_is_valid_postfix_expression_returns_true_for_empty_parentheses(self):
+        self.assertTrue(
+            parser.is_valid_postfix_expression(parser.parse("()")))
+
+    def test_is_valid_postfix_expression_returns_false_for_unbalanced_parentheses(self):
+        self.assertFalse(
+            parser.is_valid_postfix_expression(parser.parse("(()")))
+        self.assertFalse(
+            parser.is_valid_postfix_expression(parser.parse("()(")))
+        self.assertFalse(parser.is_valid_postfix_expression(
+            parser.parse("()(()")))
+
+
+class TestExecuteOperation(unittest.TestCase):
+    def test_execute_operation_addition(self):
+        self.assertEqual(1, parser.execute_operation("+", 1, 0))
+        self.assertEqual(1, parser.execute_operation("+", 0, 1))
+        self.assertEqual(2, parser.execute_operation("+", 1, 1))
+        self.assertEqual(27, parser.execute_operation("+", 26, 1))
+
+    def test_execute_operation_subtraction(self):
+        self.assertEqual(1, parser.execute_operation("-", 1, 0))
+        self.assertEqual(0, parser.execute_operation("-", 0, 0))
+        self.assertEqual(-1, parser.execute_operation("-", 0, 1))
+
+    def test_execute_operation_multiplication(self):
+        self.assertEqual(0, parser.execute_operation("*", 1, 0))
+        self.assertEqual(2, parser.execute_operation("*", 1, 2))
+        self.assertEqual(52, parser.execute_operation("*", 26, 2))
+
+    def test_execute_operation_integer_division(self):
+        self.assertEqual(1, parser.execute_operation("/", 1, 1))
+        self.assertEqual(-1, parser.execute_operation("/", 1, -1))
+        self.assertEqual(1, parser.execute_operation("/", 2, 2))
+        self.assertEqual(4, parser.execute_operation("/", 9, 2))
+        with self.assertRaises(ZeroDivisionError):
+            parser.execute_operation("/", 1, 0)
+
+    def test_execute_operation_exponentiation(self):
+        self.assertEqual(1, parser.execute_operation("$", 0, 0))
+        self.assertEqual(0, parser.execute_operation("$", 0, 1))
+        self.assertEqual(0, parser.execute_operation("$", 0, 2))
+        self.assertEqual(1, parser.execute_operation("$", 1, 0))
+        self.assertEqual(1, parser.execute_operation("$", 2, 0))
+        self.assertEqual(1, parser.execute_operation("$", 1, 2))
+        self.assertEqual(4, parser.execute_operation("$", 2, 2))
+        self.assertEqual(16, parser.execute_operation("$", 4, 2))
+
+
 class TestPostfixEvaluation(unittest.TestCase):
     def test_evaluate_postfix_returns_none_on_empty_string(self):
         expression = ""
