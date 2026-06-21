2b9fd9ace92a95266270e5087dcd765fbd1f0aab
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-06-21 18:09:31 -0400

    Rewire process_files and include error messages

--- lab1/lab1/lab1.py
+++ lab1/lab1/lab1.py
@@ -15,17 +15,32 @@ def process_files(input_file: TextIO, output_file: TextIO) -> None:
     :param output_file: An opened text file set to write mode
     """
     logging.info("called process_files")
+    lines = input_file.read().splitlines()
+    errors = []
+    for line in lines:
+        if line is not None and line != "":
+            try:
+                postfix = converter.pre2post(line)
+            except ValueError as e:
+                errors.append(f"{e}")
+                continue
+            output_file.write(postfix)
+            output_file.write("\n")
+    if errors:
+        error_preamble = [
+f"""WARNING: {len(errors)} errors found during conversion
+WARNING: Invalid expressions not written to output file\n
+Check expressions to ensure the are well formed and only use allowed symbols.\n
+Note: whitespace is not allowed. Expressions with whitespace will be rejected.\n
+Allowed symbols: ABCDEFGHIJKLMNOPQRSTUVWXYZ+-*/$()\n
+Example valid expressions:
+\t(base case):\t'A'\t-> '{converter.pre2post('A')}'
+\t(single op):\t'+AB'\t-> '{converter.pre2post('+AB')}'
+\t(more ops):\t'-+ABC'\t-> '{converter.pre2post('-+ABC')}'""",
+            f"-"*80
+        ]
+        for msg in error_preamble:
+            print(msg, file=stderr)
 
-    next_line = input_file.readline()
-    while next_line is not None and next_line != "":
-        try:
-            expression = next_line
-        except ValueError as e:
-            print(e, file=stderr)
-            continue
-        finally:
-            next_line = input_file.readline()
-
-        postfix = converter.pre2post(expression)
-        output_file.write(postfix)
-        output_file.write("\n")
+        for error in errors:
+            print(error, file=stderr)
