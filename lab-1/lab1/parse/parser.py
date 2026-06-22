a6b87b2d9695afcd746b93a48cf5274ce1be7686
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-06-22 19:22:52 -0400

    Add docstring for execute_operation

--- lab-1/lab1/parse/parser.py
+++ lab-1/lab1/parse/parser.py
@@ -72,8 +72,38 @@ def parse(expression: str, translate_symbols: bool = False) -> list:
     return parsed_symbols
 
 
-def execute_operation(s: str, a: int, b: int) -> int:
-    match s:
+def execute_operation(op: str, a: int, b: int) -> int:
+    """Executes the operation provided in op on the operands a and b
+
+    Returns an int resulting from the evaluation of the operation represented
+    by the str op on the int operands a and b. For example, if op == "+",
+    then the result of a + b will be returned. Supported operations are those
+    from the specification of the lab, viz. +, -, *, /, $. This is provided
+    to support the evaluation of the expressions as a validation step.
+    Raises a ValueError if the operation op provided is not recognized.
+    
+    A few notes:
+    / represents integer division, not floating point division.
+    $ represents exponentiation
+
+    Args:
+        op: str where op is one of the operations +, -, *, /, $
+        a: int where a is an operand from an expression 
+        b: int where b is an operand from an expression 
+
+    Returns:
+        int result of the evaluation of a op b
+
+    Raises:
+        ValueError: if the expression contains illegal characters
+
+    Side Effects:
+        Raises ValueError as described above
+
+    Idempotent:
+        True
+    """
+    match op:
         case "+":
             return a + b
         case "-":
@@ -85,4 +115,4 @@ def execute_operation(s: str, a: int, b: int) -> int:
         case "$":
             return a ** b
     raise ValueError(
-        f"Unable to execute operation {a}{s}{b}, because {s} was not recognized")
+        f"Unable to execute operation {a}{op}{b}, because {op} was not recognized")
