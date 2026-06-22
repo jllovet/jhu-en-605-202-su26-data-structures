23f9f61cc3bb6b2fc008658df42760671ac1ad78
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-06-22 01:13:31 -0400

    Excise parentheses handling from conversion because unneeded

--- lab-1/lab1/convert/converter.py
+++ lab-1/lab1/convert/converter.py
@@ -1,13 +1,16 @@
 from lab1.stack.stack import Stack
 import lab1.parse.prefix as prefix
 import lab1.parse.parser as parser
+import lab1.parse.validate as validate
 
 
 def pre2post(expression: str) -> str:
     if not prefix.is_valid(expression):
         raise ValueError(f"'{expression}' is not a valid prefix expression")
+    # Passing here -> parentheses are balanced
+    # We actually don't need to handle them at all for this conversion,
+    # since precedence is explicit in prefix and postfix expressions.
     stack = Stack()
-    parentheses_stack = Stack()
     parsed_expression = parser.parse(expression=expression)
     postfix_expression_components = []
     if parsed_expression == [] or len(parsed_expression) == 1:
@@ -15,14 +18,9 @@ def pre2post(expression: str) -> str:
     for i in range(0, len(parsed_expression)):  # iterate from right
         index = len(parsed_expression) - (i + 1)
         s = parsed_expression[index]
-        if s == ")":
-            parentheses_stack.push(s)
-        if s == "(":
-            # We don't need to check that top is ")",
-            # because we already checked that this is a valid expression,
-            # which implies that the parentheses are balanced.
-            _ = parentheses_stack.pop()
-        if s == "+" or s == "-" or s == "/" or s == "*" or s == "$":
+        if validate.is_parentheses(s):
+            continue
+        if validate.is_operation(str(s)):
             a = stack.pop()
             b = stack.pop()
             stack.push(f"{a}{b}{s}")
