09cd60a36e500eb0ce12bc1490dd291c776c54f8
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-06-22 23:53:09 -0400

    Adapt comments on __main__

--- lab-1/lab1/__main__.py
+++ lab-1/lab1/__main__.py
@@ -5,22 +5,22 @@
 # imports. This whole file is basically the java equivalent of:
 # public static void main(string args[]), or c's int main();
 
-# Generally used to process command line arguments and 'launch' the program
 from pathlib import Path
 import argparse
 
 from lab1.lab1 import process_files
 
-# Argument parser is an amazing tool. It's worth mastering
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("in_file", type=str, help="Input File Pathname")
 arg_parser.add_argument("out_file", type=str, help="Output File Pathname")
 args = arg_parser.parse_args()
 
-# pathlib.Path is also a fantastic built in tool and has a lot of great
-# features. Please look it up! I promise it's worth it.
 in_path = Path(args.in_file)
 out_path = Path(args.out_file)
 
+# Here's the real entrypoint into the whole program. Reads the input file,
+# performs the conversions, and then writes errors to stderr and the
+# successful results to the output file. The input and output files are
+# read from the command line input above.
 with in_path.open('r') as input_file, out_path.open('w') as output_file:
     process_files(input_file, output_file)
