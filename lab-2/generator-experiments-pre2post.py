e55c146d701d8ddccaf8640f85c097204dbc15e5
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-07-13 15:54:55 -0400

    Add generator experiments with partial recursive implementation

--- lab-2/generator-experiments-pre2post.py
+++ lab-2/generator-experiments-pre2post.py
@@ -1,14 +1,134 @@
-from typing import Generator
+from typing import Tuple, Optional
 
-def _pre2post(expression: str, index:int) -> Generator[str]:
-    while index < len(expression):
-        yield expression[index]
+
+def is_operator(symbol: str) -> bool:
+    """Returns a boolean indicating whether the symbol is an operator
+
+    Returns true if the symbol is in one of the operators: +, -, *, /, $
+
+    Args:
+        symbol: str a single character from an expression
+
+    Returns:
+        A bool indicating whether the symbol is an operator
+
+    Raises:
+        None
+
+    Side Effects:
+        None
+
+    Idempotent:
+        True
+    """
+    if not isinstance(symbol, str):
+        return False
+    match symbol:
+        case "+":
+            return True
+        case "-":
+            return True
+        case "/":
+            return True
+        case "*":
+            return True
+        case "$":
+            return True
+    return False
+
+
+def is_operator_or_bracket(symbol: str) -> bool:
+    """Returns a boolean indicating whether the symbol is an operator or parenthesis
+
+    Returns true if the symbol is any of +, -, *, /, $, (, )
+
+    Args:
+        symbol: str a single character from an expression
+
+    Returns:
+        A bool indicating whether the symbol is any of +, -, *, /, $, (, )
+
+    Raises:
+        None
+
+    Side Effects:
+        None
+
+    Idempotent:
+        True
+    """
+    if not isinstance(symbol, str):
+        return False
+    return (symbol in ["+", "-", "*", "/", "$", "(", ")"])
+
+
+def is_parenthesis(symbol: str) -> bool:
+    """Returns a boolean indicating whether the symbol is "(" or ")"
+
+    Args:
+        symbol: str a single character from an expression
+
+    Returns:
+        A bool indicating whether the symbol is a parenthesis
+
+    Raises:
+        None
+
+    Side Effects:
+        None
+
+    Idempotent:
+        True
+    """
+    return symbol == "(" or symbol == ")"
+
+
+class Node:
+    def __init__(self, data, left=None, right=None):
+        self.data: str = data
+        self.left: Optional[Node] = left
+        self.right: Optional[Node] = right
+
+    def __str__(self):
+        return f"{self.data}"
+        # return f"{self.data}, left: {self.left}, right: {self.right}"
+
+    def __iter__(self):
+        if self.left:
+            yield from self.left
+        if self.right:
+            yield from self.right
+        yield self.data
+
+
+def _pre2post(expression: str, index: int = 0) -> Tuple[Node, int] | Tuple[None, int]:
+    print(f"in pre2post({expression, index})")
+    if index == len(expression):
+        return None, index
+    while index <= len(expression) - 1 and (expression[index].isspace() or is_parenthesis(expression[index])):
+        print(index)
         index += 1
+    print(
+        f"after char skip loop in pre2post({expression, len(expression), index})")
+    n = Node(expression[index])
+    print(f"created node: {n}")
+    if is_operator(expression[index]):
+        left, index = _pre2post(expression, index+1)
+        right, index = _pre2post(expression, index+1)
+        n.left = left
+        n.right = right
+    if expression[index].isalpha():
+        pass
+    return n, index
 
-a =_pre2post("abc",0)
 
-while True:
-    try:
-        print(next(a))
-    except StopIteration:
-        break
+def tree2str(tree) -> str:
+    if tree:
+        return "".join(n for n in tree)
+    else:
+        return ""
+
+
+tree, _ = _pre2post(expression="  ()) (+AB)", index=0)
+print(tree2str(tree))
+
