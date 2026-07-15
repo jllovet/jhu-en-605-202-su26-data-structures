47b6db78ee7601961cc8d7902bcba474133a166e
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-07-14 23:22:47 -0400

    Add command line options for logging

--- lab-2/lab2/__main__.py
+++ lab-2/lab2/__main__.py
@@ -8,24 +8,35 @@
 from lab2.lab2 import process_files
 from pathlib import Path
 import argparse
-
 import logging
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename='lab2.log',
-                    level=logging.DEBUG,
-                    format="%(asctime)s - [%(filename)s:%(funcName)s:%(lineno)d] - %(levelname)s - %(message)s",
-                    datefmt="%Y-%m-%dT%H:%M:%S%z")
-
 
 try:
     arg_parser = argparse.ArgumentParser(exit_on_error=False)
     arg_parser.description = f"Convert prefix -> postfix expressions from in_file and write them to out_file"
     arg_parser.add_argument("in_file", type=str, help="Input File Pathname")
     arg_parser.add_argument("out_file", type=str, help="Output File Pathname")
+    arg_parser.add_argument("-l", "--level", type=str, default="INFO",
+                            choices=["INFO", "WARNING", "ERROR",
+                                     "DEBUG", "CRITICAL", "FATAL"],
+                            help="Sets the level of the logger",
+                            required=False)
+    arg_parser.add_argument("-f", "--logfile", type=str, default="lab2.log",
+                            help="Sets the filename where logs are written",
+                            required=False)
     args = arg_parser.parse_args()
 
     in_path = Path(args.in_file)
     out_path = Path(args.out_file)
+
+    level = args.level
+    logfilename = args.logfile
+
+    logging.basicConfig(filename=logfilename,
+                        level=args.level,
+                        format="%(asctime)s - [%(filename)s:%(funcName)s:%(lineno)d] - %(levelname)s - %(message)s",
+                        datefmt="%Y-%m-%dT%H:%M:%S%z")
+
+    logger = logging.getLogger(__name__)
 
     # Here's the real entrypoint into the whole program. Reads the input file,
     # performs the conversions, and then writes errors to stderr and the
@@ -36,5 +47,5 @@ try:
             f"processing input file {in_path} and writing to {out_path}")
         process_files(input_file, output_file)
 
-except:
+except Exception as e:
     arg_parser.print_help()
