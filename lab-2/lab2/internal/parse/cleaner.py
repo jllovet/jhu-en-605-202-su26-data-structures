e0c0ee4374b17bed48edbd3cb67470a187c46c9c
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-07-14 23:28:29 -0400

    Add docstring for clean

--- lab-2/lab2/internal/parse/cleaner.py
+++ lab-2/lab2/internal/parse/cleaner.py
@@ -2,6 +2,23 @@ import lab2.internal.parse.validate as validate
 
 
 def clean(expression: str) -> str:
+    """Returns copy of expression with irrelevant characters removed
+
+    Args:
+        expression: str a candidate prefix expression
+
+    Returns:
+        A copy of expression with irrelevant characters removed
+
+    Raises:
+        None
+
+    Side Effects:
+        None
+
+    Idempotent:
+        True
+    """
     characters = []
     for s in expression:
         if validate.is_whitespace(s):
