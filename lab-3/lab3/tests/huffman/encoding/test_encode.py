71f6923ad1b81a5814aaa5e509380725fa0cc252
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-07-28 02:01:16 -0400

    Add tests for encoding frequency table

---
+++ lab-3/lab3/tests/huffman/encoding/test_encode.py
@@ -0,0 +1,30 @@
+import unittest
+import lab3.huffman.encoding.encode as encode
+from lab3.huffman.encoding.encode import EncodingData
+
+
+class TestFrequencyListToEncodingData(unittest.TestCase):
+    def test_single_element(self):
+        mock_file = ["A - 1"]
+        manual = [EncodingData(characters="A", score=1)]
+        self.assertListEqual(manual, encode.frequency_to_encoding_data(mock_file))
+
+    def test_unclean_data(self):
+        mock_file = ["A - 1 "]
+        manual = [EncodingData(characters="A", score=1)]
+        self.assertListEqual(manual, encode.frequency_to_encoding_data(mock_file))
+
+        mock_file = ["A - 1 2"]
+        manual = [EncodingData(characters="A", score=12)]
+        with self.assertRaises(ValueError):
+            encode.frequency_to_encoding_data(mock_file)
+
+        mock_file = ["A.B - 1B2"]
+        manual = [EncodingData(characters="A", score=12)]
+        with self.assertRaises(ValueError):
+            encode.frequency_to_encoding_data(mock_file)
+
+        mock_file = ["A.B - 1B2", "", {False}]
+        manual = [EncodingData(characters="A", score=12)]
+        with self.assertRaises(ValueError):
+            encode.frequency_to_encoding_data(mock_file)
