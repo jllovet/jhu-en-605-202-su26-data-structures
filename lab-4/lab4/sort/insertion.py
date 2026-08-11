e9e13f144018c4086452f0d5b3fea5b81af8fcee
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-08-11 08:59:43 -0400

    Add tests and baseline implementation for insertion sort

--- lab-4/lab4/sort/insertion.py
+++ lab-4/lab4/sort/insertion.py
@@ -5,4 +5,14 @@ logger = logging.getLogger(__name__)
 
 
 def sort(xs: List) -> List:
+    # Adapted from ZyBook implementation
+    for i in range(1, len(xs)):
+        j = i
+        # Insert xs[i] into sorted part at correct pos
+        while j > 0 and xs[j] < xs[j - 1]:
+            # // Swap xs[j] and xs[j - 1]
+            tmp = xs[j]
+            xs[j] = xs[j - 1]
+            xs[j - 1] = tmp
+            j -= 1
     return xs
--- lab-4/lab4/tests/test_insertion.py
+++ lab-4/lab4/tests/test_insertion.py
@@ -11,3 +11,15 @@ class TestSortsProperly(unittest.TestCase):
         single_element_list = [1]
         s = insertion.sort(single_element_list)
         self.assertListEqual(s, single_element_list)
+
+    def test_sorted(self):
+        two_asc = [1, 2]
+        two_desc = [2, 1]
+        three_asc = [1, 2, 3]
+        three_desc = [3, 2, 1]
+        self.assertEqual([1, 2], insertion.sort(two_asc))
+        self.assertEqual([1, 2, 3], insertion.sort(three_asc))
+        self.assertEqual(sorted(two_asc), insertion.sort(two_asc))
+        self.assertEqual(sorted(two_desc), insertion.sort(two_desc))
+        self.assertEqual(sorted(three_asc), insertion.sort(three_asc))
+        self.assertEqual(sorted(three_desc), insertion.sort(three_desc))
