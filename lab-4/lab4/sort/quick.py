27fe92eb333e6d9c9de291b6db140aae5b1c6ec2
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-08-11 08:47:41 -0400

    Add median of three method and tests

--- lab-4/lab4/sort/quick.py
+++ lab-4/lab4/sort/quick.py
@@ -1,8 +1,16 @@
-from typing import List
+from typing import List, Tuple
+import lab4.sort.insertion as insertion
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-def sort(xs: List) -> List:
+def sort(xs):
     return xs
+
+
+def median_of_three(a: int, b: int, c: int) -> int:
+    items = [a, b, c]
+    items.remove(min(items))
+    items.remove(max(items))
+    return items[0]
--- lab-4/lab4/tests/test_quick.py
+++ lab-4/lab4/tests/test_quick.py
@@ -11,4 +11,21 @@ class TestSortsProperly(unittest.TestCase):
             single_element_list = [1]
             s = quick.sort(single_element_list)
             self.assertListEqual(s, single_element_list)
-    
+
+
+class TestMedianOfThree(unittest.TestCase):
+    def test_median_of_three_ascending(self):
+        self.assertEqual(quick.median_of_three(1, 2, 3), 2)
+        self.assertEqual(quick.median_of_three(1, 2, 2), 2)
+        self.assertEqual(quick.median_of_three(2, 2, 2), 2)
+
+    def test_median_of_three_descending(self):
+        self.assertEqual(quick.median_of_three(3, 2, 1), 2)
+        self.assertEqual(quick.median_of_three(3, 1, 1), 1)
+        self.assertEqual(quick.median_of_three(3, 3, 3), 3)
+
+    def test_median_of_three_random(self):
+        self.assertEqual(quick.median_of_three(1, 3, 2), 2)
+        self.assertEqual(quick.median_of_three(2, 3, 1), 2)
+        self.assertEqual(quick.median_of_three(2, 1, 3), 2)
+        self.assertEqual(quick.median_of_three(2, 2, 3), 2)
