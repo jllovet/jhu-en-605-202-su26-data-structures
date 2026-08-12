acda554d0c8f8ac5a89dd820e03ab3bac4f9297c
Jonathan Llovet <jonathan.llovet@gmail.com>
2026-08-11 22:50:49 -0400

    Update insertion sort to use context

--- lab-4/lab4/sort/insertion.py
+++ lab-4/lab4/sort/insertion.py
@@ -1,18 +1,23 @@
-from typing import List
+from lab4.sort.context import Context
+from typing import List, Tuple
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-def sort(xs: List) -> List:
+def sort(context: Context, xs: List) -> Tuple[Context, List[int]]:
     # Adapted from ZyBook implementation
+    logger.debug("in insertion.sort")
     for i in range(1, len(xs)):
         j = i
         # Insert xs[i] into sorted part at correct pos
         while j > 0 and xs[j] < xs[j - 1]:
             # // Swap xs[j] and xs[j - 1]
+            logger.debug(
+                f"swapping xs[{j}] and xs[{j} - 1]: {xs[j]} and {xs[j - 1]}")
+            context.comparisons += 1
             tmp = xs[j]
             xs[j] = xs[j - 1]
             xs[j - 1] = tmp
             j -= 1
-    return xs
+    return context, xs
--- lab-4/lab4/tests/test_insertion.py
+++ lab-4/lab4/tests/test_insertion.py
@@ -1,15 +1,20 @@
 import unittest
 import lab4.sort.insertion as insertion
+from lab4.sort.context import Context, FirstItemPivotInsertionSortForPartitionsLE100
 
 
 class TestSortsProperly(unittest.TestCase):
     def test_empty(self):
-        s = insertion.sort([])
+        xs = []
+        context = Context(FirstItemPivotInsertionSortForPartitionsLE100, xs=xs)
+        context, s = insertion.sort(context, xs)
         self.assertListEqual(s, [])
 
     def test_single_element_list(self):
         single_element_list = [1]
-        s = insertion.sort(single_element_list)
+        context = Context(
+            FirstItemPivotInsertionSortForPartitionsLE100, xs=single_element_list)
+        context, s = insertion.sort(context, single_element_list)
         self.assertListEqual(s, single_element_list)
 
     def test_sorted(self):
@@ -17,9 +22,23 @@ class TestSortsProperly(unittest.TestCase):
         two_desc = [2, 1]
         three_asc = [1, 2, 3]
         three_desc = [3, 2, 1]
-        self.assertEqual([1, 2], insertion.sort(two_asc))
-        self.assertEqual([1, 2, 3], insertion.sort(three_asc))
-        self.assertEqual(sorted(two_asc), insertion.sort(two_asc))
-        self.assertEqual(sorted(two_desc), insertion.sort(two_desc))
-        self.assertEqual(sorted(three_asc), insertion.sort(three_asc))
-        self.assertEqual(sorted(three_desc), insertion.sort(three_desc))
+        dummy_context = Context(
+            FirstItemPivotInsertionSortForPartitionsLE100, xs=[])
+
+        self.assertEqual([1, 2],
+                         insertion.sort(dummy_context, two_asc)[1])
+
+        self.assertEqual([1, 2, 3],
+                         insertion.sort(dummy_context, three_asc)[1])
+
+        self.assertEqual(sorted(two_asc),
+                         insertion.sort(dummy_context, two_asc)[1])
+
+        self.assertEqual(sorted(two_desc),
+                         insertion.sort(dummy_context, two_desc)[1])
+
+        self.assertEqual(sorted(three_asc),
+                         insertion.sort(dummy_context, three_asc)[1])
+
+        self.assertEqual(sorted(three_desc),
+                         insertion.sort(dummy_context, three_desc)[1])
